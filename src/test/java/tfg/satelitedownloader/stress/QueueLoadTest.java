8c59a2e65dd4f46d6c5df3df608a40e05771edb8
iazkue <iazkue@ni.eus>
2026-09-18 21:45:17 +0200

    Optimizazio aldaketak

--- src/test/java/tfg/satelitedownloader/stress/QueueLoadTest.java
+++ src/test/java/tfg/satelitedownloader/stress/QueueLoadTest.java
@@ -111,7 +111,7 @@ public class QueueLoadTest {
         boolean finished = latch.await(10, TimeUnit.SECONDS);
 
         // Verification
-        assertTrue(finished, "The asynchronous system has not been able to process the entire load ("s
+        assertTrue(finished, "The asynchronous system has not been able to process the entire load ("
                 + processedCounter.get() + "/" + NUM_REQUESTS + ")");
         verify(provider, times(NUM_REQUESTS)).downloadTile(any(Tile.class));
     }
